7b5405efbc97f9c830a2d8f015f540e6c251bc57
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-08-22 21:59:56 -0300

    change text on about

--- src/components/layout/about/index.tsx
+++ src/components/layout/about/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Container } from '@/components';
 import Image from 'next/image';
 import Background from '@/assets/carlos/psicanalista-carlos-alberto (1).jpeg';
-
+import InstagramIcon from '@/assets/icons/social-midia-instagram.svg';
+import WhatsappIcon from '@/assets/icons/social-midia-wpp.svg';
+import { CiFacebook } from "react-icons/ci";
 import './style.css';
-
+import { FaInstagram, FaWhatsapp } from 'react-icons/fa';
 const Root = () => {
+ 
   return (
     <Container.AutoScreenFullWidth>
       <Container.AutoSizeWidth className='about-content-container'>
@@ -14,7 +17,20 @@ const Root = () => {
           <h1>Quem é Carlos Alberto?</h1>
           <div className='about-content-arrow' />
           <p>Carlos Alberto é um psicanalista experiente, comprometido em ajudar seus pacientes a superar desafios emocionais e a encontrar equilíbrio em suas vidas. Com anos de prática, ele utiliza a psicanálise para explorar o inconsciente, promovendo um entendimento mais profundo de si mesmo.</p>
+          <div className='about-contact-info'>
+            <h4>Entre em contato</h4>
+            <a className='footer-contact-item' href='https://www.instagram.com/carlosalbertopsicanalista/' target='_blank'>
+              <FaInstagram />
+              <p>carlosalbertopsicanalista</p>
+            </a>
+            <a className='footer-contact-item' href='https://api.whatsapp.com/send?phone=5522988246598&text=Olá, vim do site e quero saber mais sobre seus serviços!' target='_blank'>
+              <FaWhatsapp />
+              <p>(22) 98824-6598</p>
+            </a>
+
+          </div>
         </div>
+       
       </Container.AutoSizeWidth>
     </Container.AutoScreenFullWidth>
   )
--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -2,41 +2,16 @@
 
 import React from 'react';
 import { Container } from '@/components';
-import Image from 'next/image';
-import InstagramIcon from '@/assets/icons/social-midia-instagram.svg';
-import WhatsappIcon from '@/assets/icons/social-midia-wpp.svg';
 import './style.css';
 
 const Root = () => {
     const currentYear = new Date().getFullYear()
-    const redirectToWhatsApp = (phoneNumber: string) => {
-        const url = `https://api.whatsapp.com/send/?phone=${phoneNumber}`;
-        window.open(url, '_blank');
-    };
-
-    const sendMail = (email: string) => {
-        window.location.href = email;
-    }
-
     return (
         <Container.AutoScreenFullWidth className='footer-component'>
             <Container.AutoSizeWidth className='footer-content-container'>
-                <div className='footer-left'>
-                    <p className='footer-copyright'>© {currentYear} - Carlos Alberto - Todos os direitos reservados</p>
-                </div>
-                <div className='footer-right'>
-                    <div className='footer-contact'>
-                        <h4>Entre em contato</h4>
-                        <section onClick={() => window.location.href = 'https://www.instagram.com/carlosalbertopsicanalista/'} className='footer-contact-item'>
-                            <Image src={InstagramIcon} alt='Instagram' />
-                            <p>carlosalbertopsicanalista</p>
-                        </section>
-                        <section onClick={() => redirectToWhatsApp('5522988246598')} className='footer-contact-item'>
-                            <Image src={WhatsappIcon} alt='Whatsapp' />
-                            <p>(22) 98824-6598</p>
-                        </section>
-                    </div>
-                    <p className='footer-copyright'>Desenvolvido por <a href = 'https://www.linkedin.com/in/lucas-de-sousa-dantas/' className='copyright-diagonal' target='_blank'>Lucas Dantas</a></p>
+                <div className=''>
+                  <p className='footer-copyright'>Desenvolvido por <a href = 'https://www.linkedin.com/in/lucas-de-sousa-dantas/' className='copyright-diagonal' target='_blank'>Lucas Dantas</a></p>
+                  <p className='footer-copyright'>© {currentYear} - Carlos Alberto - Todos os direitos reservados</p>
                 </div>
             </Container.AutoSizeWidth>
         </Container.AutoScreenFullWidth>
