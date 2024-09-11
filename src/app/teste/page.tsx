127ff154f9952c2ae8a6f7e1b30ad026c89c9c5a
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-09-11 07:32:21 -0300

    testing videos on vercel

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function teste() {
+    return (
+        <main>
+          <iframe width="1920" height="750" src="https://www.youtube.com/embed/XDqEvmxnLeY" title="The easiest way to add videos in NextJs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <video width="320" height="240" controls>
+            <source src="/aropodometria.mp4" type="video/mp4"/>
+            Your browser does not support the video tag.
+          </video>
+           
+        </main>
+    )
+};
