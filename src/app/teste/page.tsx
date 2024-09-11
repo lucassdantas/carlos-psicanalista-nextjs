61c8e998543bda202fa7e75179c747d13452eb40
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-09-11 08:25:50 -0300

    remove invalid attributes from iframe

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function teste() {
     return (
         <main>
-          <iframe width="1920" height="750" src="https://www.youtube.com/embed/XDqEvmxnLeY" title="The easiest way to add videos in NextJs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe width="1920" height="750" src="https://www.youtube.com/embed/XDqEvmxnLeY" title="The easiest way to add videos in NextJs" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
           <video width="320" height="240" controls>
             <source src="/aropodometria.mp4" type="video/mp4"/>
             Your browser does not support the video tag.
